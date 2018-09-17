bdf2ccae31354f9a44fe8112038fcc8290d07909
notaloney <jibiao1987@163.com>
2018-09-17 14:14:06 +0800

    汉语备注2“
    q
    
    :q
    git add .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by jibiao</div>
+                <div class="title">Hello Laravel! - by notalone</div>
             </div>
         </div>
     </body>
